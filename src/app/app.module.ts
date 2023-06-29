af53b2af37eded1e929bb8b0d79f0de9075f18fe
Juan Pablo Barrientos <jpbarrientosros@gmail.com>
2023-06-29 14:20:23 -0300

    angular version change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { AuthModule } from './modules/auth/auth.module';
 import { SharedModule } from './modules/shared/shared.module';
+import { JwtModule } from '@auth0/angular-jwt';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 import { AppComponent } from './app.component';
 import { AuthService } from './modules/auth/services/auth.service';
 import { ErrorInterceptorProvider } from './modules/auth/services/error.interceptor';
 import { AlertifyService } from './modules/shared/services/alertify.service';
 import { UserFeaturesModule } from './modules/userFeatures/userFeatures.module';
+import { UserService } from './modules/auth/services/user.service';
+import { MemberListComponent } from './modules/members/memberList/memberList.component';
+import { MemberCardComponent } from './modules/members/memberCard/memberCard.component';
+import { MemberDetailComponent } from './modules/members/memberDetail/memberDetail.component';
+
+export function tokenGetter(){
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
     AppComponent,
+    MemberListComponent,
+    MemberCardComponent,
+    MemberDetailComponent
    ],
   imports: [
     BrowserModule,
@@ -23,12 +36,20 @@ import { UserFeaturesModule } from './modules/userFeatures/userFeatures.module';
     FormsModule,
     AuthModule,
     SharedModule,
-    UserFeaturesModule
+    UserFeaturesModule,
+    JwtModule.forRoot({
+      config:{
+        tokenGetter:tokenGetter,
+        disallowedRoutes: ['localhost:5000/api/auth'],
+        allowedDomains: ['localhost:5000']
+      }
+    })
   ],
   providers: [
     AuthService,
     ErrorInterceptorProvider,
-    AlertifyService
+    AlertifyService,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
